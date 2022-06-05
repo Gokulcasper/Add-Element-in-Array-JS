38fb3d043f7c6b687229e5523235e49897173949
Gokulcasper <ronitcasper215@gmail.com>
2022-06-05 18:06:52 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// Adding Elements in Array
+
+const fruits = ["apple", "orange", "mango"];
+
+//Push
+fruits.push("Grapes");
+
+//unshift
+fruits.unshift("lemon");
+
+//splice
+fruits.splice(1, 0, "Banannas");
+
+console.log(fruits);
